7f9282682c0c0a7cccf84e3671765acdcc75a30d
garrettfarley9 <garrett.farley9@gmail.com>
2020-05-02 11:18:50 -0500

    Got the data. Now to make plots.

--- plot1.R
+++ plot1.R
@@ -2,9 +2,6 @@
 library(dplyr)
 library(readr)
 opened<-("2020-04-26 13:36:59") #Sys.time
-#h<-c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 
-file<-read_delim("household_power_consumption.txt", delim=";", na = c("", "?", "NA"))
-to_plot<- filter(file, Date == "02/01/2007" | Date == "02/02/2007")
-
-
+file<-read_delim("exdata_data_household_power_consumption.zip", ";", na = c("", "NA", "?"))
+to_plot<-filter(file, Date == "1/2/2007" | Date == "2/2/2007")
