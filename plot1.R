935c955a8277944139855868111f076c268a6dfe
garrettfarley9 <garrett.farley9@gmail.com>
2020-05-02 09:24:13 -0500

    Trying to figure out the read in to only be the dates we need.

--- plot1.R
+++ plot1.R
@@ -1,8 +1,10 @@
-rm(list=ls())
+#rm(list=ls())
 library(dplyr)
+library(readr)
 opened<-("2020-04-26 13:36:59") #Sys.time
-colname<-read.table("household_power_consumption.txt", sep = ";", nrows = 1, 
-                    stringsAsFactors = FALSE, header = TRUE)
-dat<-read.table("household_power_consumption.txt", sep = ";", 
-                skip = grep("[01]/[02]/[2007]", readLines("household_power_consumption.txt")), nrows = 2879 )
+#h<-c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+
+file<-read_delim("household_power_consumption.txt", delim=";", na = c("", "?", "NA"))
+to_plot<- filter(file, Date == "02/01/2007" | Date == "02/02/2007")
+
 
